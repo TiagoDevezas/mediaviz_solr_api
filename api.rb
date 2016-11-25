60b334f25441b42bf98adec6d657a4b7d9dde234
Tiago Devezas <tiagodevezas@gmail.com>
2016-11-25 18:07:10 +0000

    change endpoints to emulate old API

--- api.rb
+++ api.rb
@@ -3,49 +3,54 @@ require 'oj'
 require 'rsolr'
 require_relative 'helpers/helpers'
 
+before do
+  content_type :json
+  headers 'Access-Control-Allow-Origin' => '*', 
+          'Access-Control-Allow-Methods' => ['OPTIONS', 'POST', 'GET'],
+          'Access-Control-Allow-Headers' => 'Content-Type'
+end
+
 configure { set :server, :puma }
+set :protection, false
 
 Oj.default_options = {:mode => :compat }
 
 solr = RSolr.connect url: 'http://localhost:8983/solr/articles'
 
-get '/articles' do
-  content_type :json
-
+get '/items' do
   articles_query_params = {
-    "sort": "pub_date desc"
+    "sort": "pub_date desc",
+    "rows": params[:limit] ? params[:limit] : 10,
+    "start": params[:offset] ? params[:offset] : 0
   }
 
   response = solr.select params: common_query_params.merge(articles_query_params)
+  response = items_formatter(response)
   Oj.dump(response)
 end
 
-get '/groups' do
-  content_type :json
-
+get '/totals' do
   groups_query_params = {
     "fl": "null",
     "stats": true,
     "facet": true,
-    "facet.pivot": params[:by] ? "{!stats=t1}#{params[:by]}" : "{!stats=t1}date_only",
+    "facet.pivot": params[:by] && params[:by] != 'day' ? "{!stats=t1}#{params[:by]}" : "{!stats=t1}date_only",
     "stats.field": [
       "{!tag=t1 sum=true}facebook_shares",
       "{!tag=t1 sum=true}twitter_shares"
     ],
     "facet.sort": params[:by] ? "#{params[:by]} asc" : "date_only asc",
     "facet.limit": -1,
-    "facet.pivot.mincount": (params[:since] || params[:until]) && !params[:by] ? 1 : 0
+    "facet.pivot.mincount": 0
+    # "facet.pivot.mincount": (params[:since] || params[:until]) && params[:by] != 'day' ? 1 : 0
   }
 
   response = solr.select params: common_query_params.merge(groups_query_params)
+  response = totals_formatter(response, groups_query_params[:"facet.pivot"], params)
   Oj.dump(response)
 end
 
 get '/sources' do
-  content_type :json
-
-  # facet=true&facet.pivot=source_name,source_type,source_acronym&limit=-1
-
   sources_query_params = {
     "q": "*:*",
     "fl": "null",
--- helpers/helpers.rb
+++ helpers/helpers.rb
@@ -31,4 +31,32 @@ helpers do
     formatted
   end
 
+  def totals_formatter response_hash, pivot_fields, params
+    formatted = []
+    pivot_fields = pivot_fields.split('}')[1]
+    response = response_hash['facet_counts']['facet_pivot'][pivot_fields]
+    response.each do |obj|
+      formatted << Hash[
+        time: obj['value'],
+        articles: obj['count'],
+        twitter_shares: obj['stats'] ? obj['stats']['stats_fields']['twitter_shares']['sum'].to_i : 0,
+        facebook_shares: obj['stats'] ? obj['stats']['stats_fields']['facebook_shares']['sum'].to_i : 0,
+        total_shares: obj['stats'] ? obj['stats']['stats_fields']['facebook_shares']['sum'].to_i + obj['stats']['stats_fields']['twitter_shares']['sum'].to_i : 0
+      ]
+    end
+    if params[:since]
+      since_index = formatted.index { |h| h[:time] == params[:since].to_s} || nil
+      formatted = since_index ? formatted[since_index..-1] : formatted
+    end
+    if params[:until]
+      until_index = formatted.index { |h| h[:time] == params[:until].to_s} || nil
+      formatted = until_index ? formatted[0..until_index] : formatted
+    end
+    formatted
+  end
+
+  def items_formatter response_hash
+    response_hash['response']['docs']
+  end
+
 end
